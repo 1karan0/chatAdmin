2f8eb01a2289bad5e0a4ff96d013dfa7e4868caa
karan-892 <karan@codedrillinfotech.com>
2026-02-26 17:26:01 +0530

    added the images in the chatbot

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/chatbot-logo.png" />
-        <script src="https://chat-admin-silk.vercel.app/embed/c1be22dc-3454-4560-8a8f-ead6f43c4817/widget.js"></script>  
       </head>
       <body className={`${inter.className} bg-zinc-900`}><DefaultLayout>{children}</DefaultLayout></body>
     </html>
